d3ccb17de36075f281c6b97caf46625119c1f133
Alexander Zhao <zhaotianxiao722@icloud.com>
2023-05-07 01:34:56 -0400

    new commits

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Head from 'next/head'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram} from 'react-icons/ai'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram, AiFillGithub} from 'react-icons/ai'
 import Image from "next/image";
 import intro from "../public/intro.png";
 import { useState } from 'react';
@@ -19,8 +19,8 @@ export default function Home() {
       </Head>
       <main className='bg-gray-300  dark:bg-gray-800'>
         <section className="min-h-screen">
-          <nav className='py-7 flex justify-between bg-cyan-800 w-full fixed dark:bg-gray-300' >
-            <h1 className='text-2xl text-cyan-500 font-burton px-9'>Developed by Alex</h1>
+          <nav className='py-7 flex justify-between bg-cyan-800 w-full fixed' >
+            <h1 className='text-2xl text-cyan-500 font-burton px-9'>Alex . dev</h1>
             <ul className='flex items-center mr-8 '>
               <li>
               <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl text-white dark:text-black mr-8 hover:-translate-y-1' />
@@ -35,12 +35,18 @@ export default function Home() {
               <a>Projects</a>
               </li>
               <li className='mx-5 hover:-translate-y-1'>
+              <a>MakerSpace</a>
+              </li>
+              <li className='mx-5 hover:-translate-y-1'>
               <a className = 'bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-md' href="https://drive.google.com/file/d/1_jNZNPKrMCj4XdgVHAviRhQO3RX8lrPm/view?usp=share_link" target='_blank' rel="noopener noreferrer" >Resume</a>
               </li>
             </ul>
           </nav>
+          <section className='min-h-screen'>
+
+          </section>
           <div className='text-center p-5 py-36'>
-            <h2 className='text-black text-5xl py-2 font-medium '>Alexander Zhao</h2>
+            <h2 className='text-black text-5xl py-2 font-medium dark:text-white'>Alexander Zhao</h2>
             <h3 className='text-black text-2xl py-2 dark:text-white'>Fullstack and Machine Learning Developer | Computer Science Student</h3>
             <p className = 'text-gray-800 text-md py-5 leading-8 dark:text-white'>I am a self-motivated second-year undergraduate student at Queen's University studying computer science with a specialization in Software Design. I am passionate about programming and am always eager to learn and try out new technologies. I have experience in developing software applications and am also interested in machine learning and data analysis.</p>
           </div>
@@ -49,10 +55,13 @@ export default function Home() {
             <AiFillTwitterCircle className="text-blue-400 hover:rounded-full hover:shadow hover:shadow-blue-500" />
             </a>        
             <a href="https://www.instagram.com/alexzhaoooo/" target = "_blank"className='transition-opacity delay-100 hover:opacity-90 hover:-translate-y-1 '>
-            <AiFillInstagram className='hover:shadow hover:shadow-black hover:rounded-xl text-gray-500'/>
+            <AiFillInstagram className='hover:shadow hover:shadow-black hover:rounded-xl text-gray-500 dark:text-pink-400'/>
             </a>
             <a href="https://www.linkedin.com/in/alexander-zhao-926225211/" target = "_blank" className='transition-opacity delay-100 hover:opacity-90 hover:-translate-y-1' >
             <AiFillLinkedin className='hover:shadow hover:shadow-blue-700 rounded-md text-blue-700'/>
+            </a>
+            <a href="https://github.com/alexzhaoo" target = "_blank" className='transition-opacity delay-100 hover:opacity-90 hover:-translate-y-1 '>
+            <AiFillGithub className=' text-black hover:shadow hover:shadow-black rounded-full dark:text-white'/>
             </a>
           </div>
           <div className='mx-auto my-10 bg-gradient-to-b from-gray-600 to-orange-100 rounded-full w-72 h-50 overflow-hidden '>
@@ -65,7 +74,7 @@ export default function Home() {
         <section className="min-h-screen text-center">
           <div className='text-black '>
             <h3 className=' text-3xl py-1 text-center dark:text-white'>My Journey as a Developer</h3>
-            <p className='text-md py-5 leading-8 text-gray-800'> A <span className='text-teal-500'> blurb</span> this is how u highlight text btw about experience, and projects , and school.</p>
+            <p className='text-md py-5 leading-8 text-gray-800 dark:text-white'> A <span className='text-teal-500'> blurb</span> this is how u highlight text btw about experience, and projects , and school.</p>
           </div>
           <div>
             <div>
